2c7e506af2539217257d8ffbf7f029fe7d89410c
Pablo Faz <pablo.faz.meza@gmail.com>
2023-10-05 22:08:20 -0600

    1er componente creado NavBar

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <header>
+        <h1>Canyon</h1>
+
+        <nav>
+            <ul>
+                <li>Montaña</li>
+                <li>Gravel</li>
+                <li>Ruta</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
