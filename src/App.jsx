4e4fc2814f1980f35269dc34c960397b1f670474
Pablo Faz <pablo.faz.meza@gmail.com>
2023-10-27 19:07:34 -0600

    agregamos components

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <NavBar/>
-      <ItemListContainer greeting = "¡Hola Amikos!" />
+      <ItemListContainer greeting = "¡Hola Amikosss!" />
     </>
   )
 }
